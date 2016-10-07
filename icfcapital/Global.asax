61a7b506cceb0a2f37f71617d3641401591c4ec9
Ross Tierney <rossgtierney@gmail.com>
2016-10-07 03:23:38 +0100

    Add project files.

---
+++ icfcapital/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="icfcapital.MvcApplication" Language="C#" %>
---
+++ icfcapital/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(icfcapital.Startup))]
+namespace icfcapital
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
